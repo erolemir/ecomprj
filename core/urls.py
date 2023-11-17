39fa1244d2efcd0517100d1e9084d9792457a9c5
erolemir <emirhaanerol@gmail.com>
2023-11-17 16:56:46 +0300

    "İndex ve shop sayfası dinamik"

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from core.views import index
+from core.views import index,shop
 
 
 
 app_name = "core"
 
 urlpatterns = [
-    path('',index,name="index")
+    path('',index,name="index"),
+    path('shop/',shop,name="shop"),
 ]
 
--- core/views.py
+++ core/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render,HttpResponse
+from core.models import Category,CartOrderItems,Address,CartOrder,Wishlist,Vendor,Product,ProductImages,ProductReview
 
 # Create your views here.
 
 def index(request):
-    return render(request,'core/index.html')
+    toprak = Category.objects.get(title="Toprakta Yetişen")
+    products = Product.objects.filter(category=toprak)[:3]
+    context = {
+        'products':products
+    }
+    return render(request,'core/index.html',context)
+
+def shop(request):
+    products = Product.objects.filter(product_status="published")
+    context = {
+        "products":products,
+    }
+    return render(request, 'core/shop.html',context)
+
